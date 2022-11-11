14eacf91dc7f2ad843c08b629b55cd5a942a1070
Matvei <32172257+DrankRock@users.noreply.github.com>
2022-11-11 21:54:44 +0100

    nicer output

--- autoCheck.sh
+++ autoCheck.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+GREEN='\033[0;32m'
+RED='\033[0;31m'
+NC='\033[0m' # No Color # Source --> https://stackoverflow.com/a/5947802
+
 if [ $# -ne 1 ]
 then
 	echo "Usage : autoCheck.sh <your_csv_file>"
@@ -7,7 +11,14 @@ fi
 i=1
 while read line; do
 	lineCut=$(echo $line | sed 's/,/ /g')
-	echo "[${i}] -- ${lineCut}"
-	python3 calendarOverlap.py -c $lineCut
+	# echo -e "${RED} [${i}] -- ${lineCut} ${NC} --> $result"
+	# echo "[${i}] -- ${lineCut}"
+	result=$(python3 calendarOverlap.py -c ${lineCut})
+
+	if [[ ${result} ]]; then
+		echo -e "${RED}[${i}] -- ${lineCut} ${NC} --> $result"
+	else
+		echo -e "${GREEN}[${i}] -- ${lineCut} ${NC}"
+	fi
 	i=$((i+1))
 done < $1
